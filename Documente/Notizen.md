efec5eb52904e42f7e2d5da2935cfb06fa64a2bd
MIRACULIX\Irallia <irallia@chrigelyra.de>
2017-06-15 11:37:04 +0200

    add some notes; prepare phyloTree with ottIds
    Former-commit-id: dc06177f931bfc5f37f636f577d503d6ab8d7e37

---
+++ Documente/Notizen.md
@@ -0,0 +1,13 @@
+# Algorithmen / Methoden
+* Parsimony
+* Likelihood / Bayesian
+
+# Programme
+
+* ``MacClade``: computer program for phylogenetic analysis ... Its analytical strength is in studies of character evolution. ...
+    ```
+    http://macclade.org/macclade.html
+    ```
+    (Paper: Compositional bias, character-state bias and character-state reconstruction using parsimony)
+    compositional bias: the occurence of the properties in unequal proportions
+
--- newick2json/prepareJson.js
+++ newick2json/prepareJson.js
@@ -1,57 +1,72 @@
 var fs = require("fs");
 var Newick = require('newick');
+// var data = fs.readFileSync("../opentree9.1_tree/grafted_solution/grafted_solution_ottnames.tre").toString();
+var data = fs.readFileSync("../opentree9.1_tree/labelled_supertree/labelled_supertree_ottnames.tre").toString();
 
-var data = fs.readFileSync("../opentree9.1_tree/grafted_solution/grafted_solution_ottnames.tre").toString();
 var parsedData = Newick.parse(data);
 
+console.log(parsedData);
 
+console.log("#otts =", JSON.stringify(parsedData).split("ott").length)
+
+function readOttIds(obj, maxId) {
+    // The == operator will compare for equality after doing any necessary type conversions. The === operator will not do the conversion, so if two values are not the same type === will simply return false. Both are equally quick.
+    if (obj.name && obj.name != "") {
+        var currentId = parseInt(obj.name.split("ott")[1])
+        if (currentId > maxId) {
+            maxId = currentId;
+        }
+    }
+    if (obj.branchset) {
+        if (obj.branchset.length > 50) {
+            console.log("# children=", obj.branchset.length);
+        }
+        var i = 0;
+        while (i < obj.branchset.length) {
+            // go deeper:
+            maxId = readOttIds(obj.branchset[i], maxId);
+            i++;
+        }
+    } // else is leaf
+    return maxId
+}
+
+maxId = readOttIds(parsedData, 0);
+console.log("maxId =", maxId);
 
 // delete Bacteria_ott844192 and Archaea_ott996421:
 var preparedData = parsedData.branchset[0]
 
-console.log(preparedData);
-
-function readOttIds(branchset, values) {
-    var maxId = values[0]
-    var counter = values[1]
-    console.log(branchset.length)
-    for (i = 0; i < branchset.length; i++) {
-        if (i>0) {console.log("i=",i)}
-        // The == operator will compare for equality after doing any necessary type conversions. The === operator will not do the conversion, so if two values are not the same type === will simply return false. Both are equally quick.
-        if (branchset[i].name === "" || branchset[i].name == undefined) {
-            console.log("no name")
-        } else {
-            var currentId = parseInt(branchset[i].name.split("ott")[1])
-            console.log(currentId)
-            if (currentId > maxId) {
-                maxId = currentId;
-                console.log("maxId=", maxId);
-            }
-        }
-        if (branchset[i].branchset) {
+function setOttIds(obj, nextId) {
+    // The == operator will compare for equality after doing any necessary type conversions. The === operator will not do the conversion, so if two values are not the same type === will simply return false. Both are equally quick.
+    if (obj.name == undefined) {
+        obj[name] = "ott" + nextId;
+        nextId++;
+    } else if (obj.name === "") {
+        obj.name = "ott" + nextId;
+        nextId++;
+    }
+    if (obj.branchset) {
+        var i = 0;
+        while (i < obj.branchset.length) {
             // go deeper:
-            values = readOttIds(branchset[i].branchset, [maxId, counter+1]);
-        } else {
-            values = [maxId, counter+1] 
-            console.log("------------------------------------------------------------");
-            console.log("is leaf: ")
-            console.log(branchset);
-            console.log("------------------------------------------------------------");
+            nextId = setOttIds(obj.branchset[i], nextId);
+            i++;
         }
     }
-    console.log("counter=",counter)
-    return values
+    return nextId;
 }
 
-readOttIds(preparedData.branchset, [0,0])
+nextId = setOttIds(preparedData, maxId + 1);
 
+console.log("nextId - maxId =", nextId - maxId);
+console.log("#otts Eukaryota =", JSON.stringify(preparedData).split("ott").length)
 
 // var jsonData = JSON.stringify(data2, null, 4); // Indented 4 spaces
 var jsonData = JSON.stringify(preparedData);
-console.log("#ottIds: ", jsonData.split("ott").length)
 
-fs.writeFile("grafted_solution_ottnames-prepared.json", jsonData, function(err) {
-    if(err) {
+fs.writeFile("labelled_supertree_ottnames-prepared.json", jsonData, function (err) {
+    if (err) {
         return console.log(err);
     }
 });
