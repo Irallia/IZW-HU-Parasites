ca82248262589f1aaeefa1ba6b9d8401555e5b39
MIRACULIX\Irallia <irallia@chrigelyra.de>
2017-06-22 10:17:52 +0200

    build tsv
    Former-commit-id: 22edad13169d3baefde0c0992745d362c33c88bb

--- newick2json/prepareJson.js
+++ newick2json/prepareJson.js
@@ -18,9 +18,9 @@ function readOttIds(obj, maxId) {
         }
     }
     if (obj.branchset) {
-        if (obj.branchset.length > 50) {
-            console.log("# children=", obj.branchset.length);
-        }
+        // if (obj.branchset.length > 50) {
+        //     console.log("# children=", obj.branchset.length);
+        // }
         var i = 0;
         while (i < obj.branchset.length) {
             // go deeper:
@@ -36,8 +36,16 @@ console.log("maxId =", maxId);
 
 // delete Bacteria_ott844192 and Archaea_ott996421:
 var preparedData = parsedData.branchset[0]
+var graphFormat = {
+    nodes: [],
+    edges: []
+}
+// nodes.tsv
+var nodesTsv = ["ID\tname\n"];
+// edges.tsv
+var edgesTsv = ["_from\t_to\n"];
 
-function setOttIds(obj, nextId) {
+function setOttIds(obj, nextId, graphFormat, nodesTsv, edgesTsv) {
     // The == operator will compare for equality after doing any necessary type conversions. The === operator will not do the conversion, so if two values are not the same type === will simply return false. Both are equally quick.
     if (obj.name == undefined) {
         obj[name] = "ott" + nextId;
@@ -46,18 +54,34 @@ function setOttIds(obj, nextId) {
         obj.name = "ott" + nextId;
         nextId++;
     }
+    // Fill graphFormat nodes:
+    graphFormat.nodes.push({id: obj.name})
+    var id = "ott" + obj.name.split("ott")[1];
+    var name = obj.name.split("ott")[0].slice(0, -1);
+    nodesTsv.push(id, "\t", name, "\n");
     if (obj.branchset) {
         var i = 0;
         while (i < obj.branchset.length) {
             // go deeper:
-            nextId = setOttIds(obj.branchset[i], nextId);
+            data = setOttIds(obj.branchset[i], nextId, graphFormat, nodesTsv, edgesTsv);
+            nextId = data[0];
+            graphFormat = data[1];
+            nodesTsv = data[2];
+            edgesTsv = data[3];
+            // Fill graphFormat edges:
+            graphFormat.edges.push({_from: obj.name, _to: obj.branchset[i].name});
+            data[3].push("ott", obj.name.split("ott")[1], "\t", "ott", obj.branchset[i].name.split("ott")[1] + "\n");
             i++;
         }
     }
-    return nextId;
+    return [nextId, graphFormat, nodesTsv, edgesTsv];
 }
 
-nextId = setOttIds(preparedData, maxId + 1);
+var data = setOttIds(preparedData, maxId + 1, graphFormat, nodesTsv, edgesTsv);
+var nextId = data[0]
+graphFormat = data[1]
+nodesTsv = data[2]
+edgesTsv = data[3]
 
 console.log("nextId - maxId =", nextId - maxId);
 console.log("#otts Eukaryota =", JSON.stringify(preparedData).split("ott").length)
@@ -65,10 +89,31 @@ console.log("#otts Eukaryota =", JSON.stringify(preparedData).split("ott").lengt
 // var jsonData = JSON.stringify(data2, null, 4); // Indented 4 spaces
 var jsonData = JSON.stringify(preparedData);
 
+// fs.writeFile("grafted_solution_ottnames-prepared.json", jsonData, function (err) {
 fs.writeFile("labelled_supertree_ottnames-prepared.json", jsonData, function (err) {
     if (err) {
         return console.log(err);
     }
 });
 
+// var jsonGraphData = JSON.stringify(graphFormat);
+// // fs.writeFile("grafted_solution_ottnames-graph_prepared.json", jsonGraphData, function (err) {
+// fs.writeFile("labelled_supertree_ottnames-graph_prepared.json", jsonGraphData, function (err) {
+//     if (err) {
+//         return console.log(err);
+//     }
+// });
 
+// fs.writeFile("grafted_solution_ottnames-nodes.tsv", nodesTsv.join(''), function (err) {
+fs.writeFile("labelled_supertree_ottnames-nodes.tsv", nodesTsv, function (err) {
+    if (err) {
+        return console.log(err);
+    }
+});
+
+// fs.writeFile("grafted_solution_ottnames-edges.tsv", edgesTsv.join(''), function (err) {
+fs.writeFile("labelled_supertree_ottnames-edges.tsv", edgesTsv, function (err) {
+    if (err) {
+        return console.log(err);
+    }
+});
