21f4154d2f670ad03e197116e44e24efaa18067e
MIRACULIX\Irallia <irallia@chrigelyra.de>
2017-08-31 14:13:48 +0200

    test some fixes
    Former-commit-id: 2a2adffc12d43c2347be013a3f638c75b68d5508

--- scripts/marius/tag_interactions.js
+++ scripts/marius/tag_interactions.js
@@ -4,56 +4,64 @@ const db = require('arangojs')();
 //tag parasites source
 
 db.query(`FOR doc IN interaction_tsv
-          FILTER doc.interactionTypeName == "parasiteOf" ||
-          doc.interactionTypeName == "ectoParasiteOf" ||
-          doc.interactionTypeName == "kleptoparasiteOf" ||
-          doc.interactionTypeName == "ectoParasitoid" ||
-          doc.interactionTypeName == "endoparasiteOf" ||
-          doc.interactionTypeName == "parasitoidOf" ||
-          doc.interactionTypeName == "endoparasitoidOf" ||
-          doc.interactionTypeName == "pathogenOf"
-          UPDATE doc WITH { parasite: 1,
-                            directionP: "source",
-                            pname: doc.sourceTaxonName } IN interaction_tsv`)
+    FILTER doc.interactionTypeName == "parasiteOf" ||
+    doc.interactionTypeName == "ectoParasiteOf" ||
+    doc.interactionTypeName == "kleptoparasiteOf" ||
+    doc.interactionTypeName == "ectoParasitoid" ||
+    doc.interactionTypeName == "endoparasiteOf" ||
+    doc.interactionTypeName == "parasitoidOf" ||
+    doc.interactionTypeName == "endoparasitoidOf" ||
+    doc.interactionTypeName == "pathogenOf"
+    UPDATE "${doc._key}" WITH {
+        parasite: 1,
+        directionP: "source",
+        pname: doc.sourceTaxonName
+    } IN interaction_tsv`);
 
 //tag parasites target
 
-db.query(`  FOR doc IN interaction_tsv
-            FILTER doc.interactionTypeName == "hasParasite" ||
-            doc.interactionTypeName == "hasPathogen"
-            UPDATE doc WITH {   parasite: 1,
-                                directionP: "target",
-                                pname: doc.targetTaxonName } IN interaction_tsv`)
+db.query(`FOR doc IN interaction_tsv
+    FILTER doc.interactionTypeName == "hasParasite" ||
+    doc.interactionTypeName == "hasPathogen"
+    UPDATE "${doc._key}" WITH {
+        parasite: 1,
+        directionP: "target",
+        pname: doc.targetTaxonName
+    } IN interaction_tsv`);
 
 //tag freeliving source
 
-db.query(`  FOR doc IN interaction_tsv
-            FILTER doc.interactionTypeName == "visits" ||
-            doc.interactionTypeName == "preysOn" ||
-            doc.interactionTypeName == "eats" ||
-            doc.interactionTypeName == "flowersVisitedBy" ||
-            doc.interactionTypeName == "hasPathogen" ||
-            doc.interactionTypeName == "pollinatedBy" ||
-            doc.interactionTypeName == "hasParasite" ||
-            doc.interactionTypeName == "hostOf"
-            UPDATE doc WITH {   freeliving: 1,
-                                directionF: "source",
-                                fname: doc.sourceTaxonName } IN interaction_tsv`)
+db.query(`FOR doc IN interaction_tsv
+    FILTER doc.interactionTypeName == "visits" ||
+    doc.interactionTypeName == "preysOn" ||
+    doc.interactionTypeName == "eats" ||
+    doc.interactionTypeName == "flowersVisitedBy" ||
+    doc.interactionTypeName == "hasPathogen" ||
+    doc.interactionTypeName == "pollinatedBy" ||
+    doc.interactionTypeName == "hasParasite" ||
+    doc.interactionTypeName == "hostOf"
+    UPDATE "${doc._key}" WITH {
+        freeliving: 1,
+        directionF: "source",
+        fname: doc.sourceTaxonName
+    } IN interaction_tsv`);
 
 //tag freeliving target
 
-db.query(`  FOR doc IN interaction_tsv
-            FILTER doc.interactionTypeName == "preyedUponBy" ||
-            doc.interactionTypeName == "ectoParasitoid" ||
-            doc.interactionTypeName == "parasiteOf" ||
-            doc.interactionTypeName == "ectoParasiteOf" ||
-            doc.interactionTypeName == "kleptoparasiteOf" ||
-            doc.interactionTypeName == "visitsFlowersOf" ||
-            doc.interactionTypeName == "endoparasitoidOf" ||
-            doc.interactionTypeName == "parasitoidOf" ||
-            doc.interactionTypeName == "endoparasiteOf" ||
-            doc.interactionTypeName == "pathogenOf" ||
-            doc.interactionTypeName == "hasHost"
-            UPDATE doc WITH {   freeliving: 1,
-                                directionF: "target",
-                                fname: doc.targetTaxonName } IN interaction_tsv`)
+db.query(`FOR doc IN interaction_tsv
+    FILTER doc.interactionTypeName == "preyedUponBy" ||
+    doc.interactionTypeName == "ectoParasitoid" ||
+    doc.interactionTypeName == "parasiteOf" ||
+    doc.interactionTypeName == "ectoParasiteOf" ||
+    doc.interactionTypeName == "kleptoparasiteOf" ||
+    doc.interactionTypeName == "visitsFlowersOf" ||
+    doc.interactionTypeName == "endoparasitoidOf" ||
+    doc.interactionTypeName == "parasitoidOf" ||
+    doc.interactionTypeName == "endoparasiteOf" ||
+    doc.interactionTypeName == "pathogenOf" ||
+    doc.interactionTypeName == "hasHost"
+    UPDATE "${doc._key}" WITH {
+        freeliving: 1,
+        directionF: "target",
+        fname: doc.targetTaxonName
+    } IN interaction_tsv`);
