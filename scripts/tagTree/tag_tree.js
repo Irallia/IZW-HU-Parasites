012bc230873d613c4e1d3515b5788c7922638ed9
MIRACULIX\Irallia <irallia@chrigelyra.de>
2017-09-01 14:41:04 +0200

    update queries
    Former-commit-id: 32d135d20d2b201e0f8516bce0433c3271b009a5

--- scripts/tagTree/tag_tree.js
+++ scripts/tagTree/tag_tree.js
@@ -4,52 +4,56 @@ const db = require('arangojs')();
 
 console.log("tag freeliving source");
 
-db.query(`FOR doc IN interaction_tsv
-    FILTER doc.freeliving == 1 && doc.directionF == "source"
-    FOR doc2 IN nodes_otl
-        FILTER CONTAINS(doc.targetTaxonIds, CONCAT("OTT:", doc2.ott))
-        UPDATE doc2 WITH {
-            freeliving: 1,
-            directionF: "source",
-            interactionTypeNameFL: doc.interactionTypeName
-        } IN nodes_otl`
+db.query(`FOR interaction IN interaction_tsv
+    FILTER interaction.freeliving == 1 && interaction.directionF == "source"
+        FOR node IN nodes_otl
+            FILTER CONTAINS(interaction.sourceTaxonIds, CONCAT("OTT:", node._key, " ")) == true
+                UPDATE node WITH {
+                    freeliving: 1,
+                    directionF: "source",
+                    interactionTypeNameFL: interaction.interactionTypeName
+                } IN nodes_otl
+            return node`
 );
 
 console.log("tag freeliving target");
 
-db.query(`FOR doc IN interaction_tsv
-    FILTER doc.freeliving == 1 && doc.directionF == "target"
-    FOR doc2 IN nodes_otl
-        FILTER CONTAINS(doc.targetTaxonIds, CONCAT("OTT:", doc2.ott))
-        UPDATE doc2 WITH {
-            freeliving: 1,
-            directionF: "target",
-            interactionTypeNameFL: doc.interactionTypeName
-        } IN nodes_otl`
+db.query(`FOR interaction IN interaction_tsv
+FILTER interaction.freeliving == 1 && interaction.directionF == "target"
+    FOR node IN nodes_otl
+        FILTER CONTAINS(interaction.targetTaxonIds, CONCAT("OTT:", node._key, " ")) == true
+            UPDATE node WITH {
+                freeliving: 1,
+                directionF: "target",
+                interactionTypeNameFL: interaction.interactionTypeName
+            } IN nodes_otl
+        return node`
 );
 
 console.log("tag parasites source");
 
-db.query(`FOR doc IN interaction_tsv
-    FILTER doc.parasite == 1 && doc.directionP == "source"
-    FOR doc2 IN nodes_otl
-        FILTER CONTAINS(doc.targetTaxonIds, CONCAT("OTT:", doc2.ott))
-        UPDATE doc2 WITH {
-            parasite: 1,
-            directionP: "source",
-            interactionTypeNameP: doc.interactionTypeName
-        } IN nodes_otl`
+db.query(`FOR interaction IN interaction_tsv
+FILTER interaction.parasite == 1 && interaction.directionP == "source"
+    FOR node IN nodes_otl
+        FILTER CONTAINS(interaction.sourceTaxonIds, CONCAT("OTT:", node._key, " ")) == true
+            UPDATE node WITH {
+                parasite: 1,
+                directionP: "source",
+                interactionTypeNameP: interaction.interactionTypeName
+            } IN nodes_otl
+        return node`
 );
 
 console.log("tag parasites target");
 
-db.query(`FOR doc IN interaction_tsv
-    FILTER doc.parasite == 1 && doc.directionP == "target"
-    FOR doc2 IN nodes_otl
-        FILTER CONTAINS(doc.targetTaxonIds, CONCAT("OTT:", doc2.ott))
-        UPDATE doc2 WITH {
-            parasite: 1,
-            directionP: "target",
-            interactionTypeNameP: doc.interactionTypeName
-        } IN nodes_otl`
+db.query(`FOR interaction IN interaction_tsv
+FILTER interaction.parasite == 1 && interaction.directionP == "target"
+    FOR node IN nodes_otl
+        FILTER CONTAINS(interaction.targetTaxonIds, CONCAT("OTT:", node._key, " ")) == true
+            UPDATE node WITH {
+                parasite: 1,
+                directionP: "target",
+                interactionTypeNameP: interaction.interactionTypeName
+            } IN nodes_otl
+        return node`
 );
