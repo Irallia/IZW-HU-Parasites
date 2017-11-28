7bb91c5f058b86bdb579f621fb91c34a845562ea
MIRACULIX\Irallia <irallia@chrigelyra.de>
2017-11-28 17:57:37 +0100

    get interactions of GloBI - first results but still with some problems

--- scripts/R/get_interactions.R
+++ scripts/R/get_interactions.R
@@ -1,54 +1,90 @@
 library(rglobi)
 
-# List interactions identified in GloBI database
-# get_interaction_types()
-get_interaction_types(opts = list())
+path_parasites <- "../../data/interaction_data/parasite.csv"
+path_freelivings <- "../../data/interaction_data/freeliving.csv"
 
-print("-------- Parasites: --------")
-# parasites source
-# Unsupported interaction type(s): ectoParasiteOf, kleptoparasiteOf, ectoParasitoid, endoparasiteOf, parasitoidOf, endoparasitoidOf
-parasite_source <- c("parasiteOf", "pathogenOf")
-ps_data <- get_interactions_by_type(interactiontype = parasite_source)
-ps_data <- ps_data[c(1:2)]
-colnames(ps_data) <- c("taxon_external_id", "taxon_name")
+main <- function() {
+    # List interactions identified in GloBI database
+    # get_interaction_types()
+    # get_interaction_types(opts = list())
 
-# parasites target
-parasite_target <- c("hasParasite", "hasPathogen")
-pt_data <- get_interactions_by_type(interactiontype = parasite_target)
-pt_data <- pt_data[c(6:7)]
-colnames(pt_data) <- c("taxon_external_id", "taxon_name")
+    print(Sys.time())
 
-# parasite
-p_data <- rbind(ps_data, pt_data)
-nrow(ps_data)
-nrow(pt_data)
-nrow(p_data)
-p_data <- p_data[!duplicated(p_data),]
-nrow(p_data)
-write.csv(p_data, file = "data/parasite.csv")
+    print("-------- Parasites: --------")
+    # parasites source
+    # Unsupported interaction type(s): ectoParasiteOf, kleptoparasiteOf, ectoParasitoid, endoparasiteOf, parasitoidOf, endoparasitoidOf
+    parasite_source <- c("parasiteOf", "pathogenOf")
+    ps_data = get_data(parasite_source, c(1:2))
 
-print("-------- Freeliving: --------")
+    # parasites target
+    parasite_target <- c("hasParasite", "hasPathogen")
+    pt_data = get_data(parasite_target, c(6:7))
 
-# freeliving source
-# Unsupported interaction type(s): visits
-freeliving_source <- c("preysOn", "eats", "flowersVisitedBy", "hasPathogen", "pollinatedBy", "hasParasite", "hostOf")
-fs_data <- get_interactions_by_type(interactiontype = freeliving_source)
-fs_data <- fs_data[c(1:2)]
-colnames(fs_data) <- c("taxon_external_id", "taxon_name")
+    # parasite
+    p_data <- rbind(ps_data, pt_data)
+    print(nrow(ps_data))
+    print(nrow(pt_data))
+    print(nrow(p_data))
+    p_data <- p_data[!duplicated(p_data),]
+    print(nrow(p_data))
+    write.csv(p_data, file = path_parasites)
 
-# freeliving target
-# Unsupported interaction type(s): ectoParasiteOf, kleptoparasiteOf, ectoParasitoid, endoparasiteOf, parasitoidOf, endoparasitoidOf
-freeliving_target <- c("preyedUponBy", "parasiteOf", "visitsFlowersOf", "pathogenOf", "hasHost")
-ft_data <- get_interactions_by_type(interactiontype = freeliving_target)
-ft_data <- ft_data[c(6:7)]
-colnames(ft_data) <- c("taxon_external_id", "taxon_name")
+    print("-------- Freeliving: --------")
 
-# freeliving
-f_data <- rbind(fs_data, ft_data)
-nrow(fs_data)
-nrow(ft_data)
-nrow(f_data)
-f_data <- f_data[!duplicated(f_data),]
-nrow(f_data)
-write.csv(f_data, file = "data/freeliving.csv")
-print("--------------------------------------")
+    # freeliving source
+    # Unsupported interaction type(s): visits
+    freeliving_source <- c("preysOn", "eats", "flowersVisitedBy", "hasPathogen", "pollinatedBy", "hasParasite", "hostOf")
+    fs_data = get_data(freeliving_source, c(1:2))
+
+    # freeliving target
+    # Unsupported interaction type(s): ectoParasiteOf, kleptoparasiteOf, ectoParasitoid, endoparasiteOf, parasitoidOf, endoparasitoidOf
+    freeliving_target <- c("preyedUponBy", "parasiteOf", "visitsFlowersOf", "pathogenOf", "hasHost")
+    ft_data = get_data(freeliving_target, c(6:7))
+
+    # freeliving
+    f_data <- rbind(fs_data, ft_data)
+    print(nrow(fs_data))
+    print(nrow(ft_data))
+    print(nrow(f_data))
+    f_data <- f_data[!duplicated(f_data),]
+    print(nrow(f_data))
+    write.csv(f_data, file = path_freelivings)
+    print("--------------------------------------")
+    return()
+}
+
+get_data <- function(interactions, rows) {
+    limit <- 25000
+    i <- 0
+
+    otherkeys = list("limit"=limit, "skip"=i)
+    raw_data <- get_interactions_by_type(interactiontype = interactions, otherkeys = otherkeys)
+    reduced_data <- raw_data[rows]
+    data <- reduced_data[!duplicated(reduced_data),]
+
+    while(nrow(raw_data) >= limit) {
+    # while(i < 3) {
+        i <- i + 1
+        print(i)    #highest: 199
+        print(Sys.time())
+        otherkeys = list("limit"=limit, "skip"=i)
+        raw_data <- get_interactions_by_type(interactiontype = interactions, otherkeys = otherkeys)
+        reduced_data <- raw_data[rows]
+        reduced_data <- reduced_data[!duplicated(reduced_data),]
+        # print(nrow(raw_data))
+        # print(nrow(reduced_data))
+        data <- rbind(data, reduced_data)
+        print("current columns of data:")
+        print(nrow(data))
+        if(nrow(data) > 20000) {
+            print("reduce data")
+            data <- data[!duplicated(data),]
+            print(nrow(data)) # highest: 4900
+        }
+        print("--------")
+    }
+    colnames(data) <- c("taxon_external_id", "taxon_name")
+    return(data)
+}
+
+main()
