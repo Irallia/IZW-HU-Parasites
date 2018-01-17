75a724469e82615e550c5f03e7b36fc407b3f96e
MIRACULIX\Irallia <irallia@chrigelyra.de>
2018-01-17 15:12:06 +0100

    add nodelist and depths to original tree

--- scripts/get_synthesis_tree.py
+++ scripts/get_synthesis_tree.py
@@ -21,6 +21,7 @@ nr_used_parasites = 0
 unknown = 0
 internal_parasite = 0
 internal_freeliving = 0
+nodelist = []
 
 def main():
     global START_TIME
@@ -45,10 +46,12 @@ def main():
     parasites = read_tags(path_parasites)
     CURRENT_TIME = print_time(CURRENT_TIME)
     print(colored("---------------- tag tree ----------------", "green"))
-    fill_tree_with_tags(tree.clade, parasites, freelivings)
+    fill_tree_with_tags(tree.clade, parasites, freelivings, [0, 0, 0])
     print(colored(nr_leave_nodes, 'blue'), "leave nodes are in the tree")
     print(colored(nr_used_freelivings, 'blue'), "freeliving tags were used,", colored(nr_used_parasites, 'blue'), "parasite tags were used =>", colored(unknown, 'blue'), "unknown leave nodes")
     print(colored(internal_freeliving, 'blue'), "internal freeliving tags found and", colored(internal_parasite, 'blue'), "internal parasite tags found")
+    print("Rootnode, Tag, Depths: Min, Max, Mean:")
+    print(nodelist[-1])
     CURRENT_TIME = print_time(CURRENT_TIME)
     print("save tree at ../data/tagged_tree.tre")
     Phylo.write(tree, '../data/tagged_tree.tre', 'newick')
@@ -67,27 +70,34 @@ def read_tags(path):
         print('number of tag:', nr_tags)
     return tag_array
 
-def fill_tree_with_tags(subtree, parasites, freelivings):
+def fill_tree_with_tags(subtree, parasites, freelivings, depths):
     global nr_leave_nodes
     global nr_used_freelivings
     global nr_used_parasites
     global unknown
     global internal_parasite
     global internal_freeliving
+    global nodelist
+    #               [node name,   tag, depths]
+    nodelist.append([subtree.name, "", depths])
 
     if subtree.is_terminal():
         nr_leave_nodes += 1
         if get_tag(subtree.name, parasites):
-            subtree.name = "2"
             nr_used_parasites += 1
+            nodelist[-1][1] = "2"
         else:
             if get_tag(subtree.name, freelivings):
-                subtree.name = "1"
+                nodelist[-1][1] = "1"
                 nr_used_freelivings += 1
             else:
-                subtree.name = "NA"
+                nodelist[-1][1] = "NA"
                 unknown += 1
     else:
+        min_depth = 0
+        max_depth = float('inf')
+        mean_depth = 0
+        child_depth = 0
         if get_tag(subtree.name, parasites):
             internal_parasite += 1
         if get_tag(subtree.name, freelivings):
@@ -95,8 +105,25 @@ def fill_tree_with_tags(subtree, parasites, freelivings):
         # // ToDo: ? does this make any difference?
         # subtree.name = ""
         for clade in subtree.clades:
-            fill_tree_with_tags(clade, parasites, freelivings)
-    return
+            depths = fill_tree_with_tags(clade, parasites, freelivings, depths)
+            if depths[0] < min_depth:
+                min_depth = depths[0]
+            if depths[1] < max_depth:
+                max_depth = depths[1]
+            child_depth = child_depth + depths[2]
+        mean_depth = mean_depth/len(subtree.clades) + 1
+        depths = [min_depth + 1, max_depth + 1, mean_depth]
+        nodelist[-1][2] = depths
+        if min_depth > 1:
+            print(nodelist[-1][2])
+        # -------------------------------------------------
+        csv_title = "../data/nodelist.csv" 
+        fp = open(csv_title, 'a')
+        writer = csv.writer(fp)
+        writer.writerow((nodelist[-1])) 
+        fp.close()
+        # -------------------------------------------------
+    return depths
 
 def get_tag(name, species_list):
     # Checks for the presence of name in any string in the list
@@ -106,6 +133,9 @@ def get_tag(name, species_list):
         if name not in species_list:
             matching = [s for s in species_list if name in s]
             print(name, "in", matching)
+        else:
+            # The OTT ids are unambiguously
+            species_list.remove(name)
         return True
     else:
         return False
