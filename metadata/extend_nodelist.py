f71a229b8c17dfd89f07f60d86355cd9b9a452e3
MIRACULIX\Irallia <irallia@chrigelyra.de>
2018-02-08 17:29:58 +0100

    clear up

--- metadata/extend_nodelist.py
+++ metadata/extend_nodelist.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 ott_taxa = pd.read_csv('../data/interaction_data/ott_taxa.csv')
-nodelist = pd.read_csv('../data/nodelist/Eukaryota.csv')
+nodelist = pd.read_csv('../data/nodelist/Eukaryota-castor.csv')
 # nodelist = nodelist.dropna(axis=1)
 merged = ott_taxa.merge(nodelist, on='ott_id')
-merged.to_csv('../data/nodelist/Eukaryota-taxa.csv', index=False)
+merged.to_csv('../results/Eukaryota-taxa.csv', index=False)
