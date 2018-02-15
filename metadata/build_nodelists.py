5add4138b9aebe03c074a0b8258c63417293ca7e
MIRACULIX\Irallia <irallia@chrigelyra.de>
2018-02-15 13:36:12 +0100

    remove other subtrees; rename depths to heights; add depth of nodes

--- metadata/build_nodelists.py
+++ metadata/build_nodelists.py
@@ -24,6 +24,7 @@ nr_leave_nodes = 0
 nr_used_freelivings = 0
 nr_used_parasites = 0
 unknown = 0
+doubleTagged = 0
 nodelist = []
 
 def main():
@@ -38,9 +39,7 @@ def main():
     global nr_used_parasites
     global unknown
     global nodelist
-
-    subtree_names = ['Eukaryota', 'Metazoa', 'Chloroplastida', 'Fungi', 'Vertebrata', 'Tetrapoda', 
-        'Nematoda', 'Mammalia', 'Primates', 'Hominidae']
+    global doubleTagged
 
     print(colored("------------------------ build nodelists ------------------------", "green"))
     print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
@@ -52,27 +51,25 @@ def main():
     print("Parasites:")
     parasites = read_tags(path_parasites)
     CURRENT_TIME = print_time(CURRENT_TIME)
-
-    for item in subtree_names:
-        print(colored("---------------- read tree ----------------", "green"))
-        subtree_path = '../data/subtree/' + item + '.tre'
-        print("Build nodelist for:", item)
-        tree = Phylo.read(subtree_path, 'newick')
-        print(colored("---------------- tag tree ----------------", "green"))
-        current_freelivings = deepcopy(freelivings)
-        current_parasites = deepcopy(parasites)
-        fill_tree_with_tags(tree.clade, item)
-        print(colored(nr_leave_nodes, 'blue'), "leave nodes are in the tree")
-        print(colored(nr_used_freelivings, 'blue'), "freeliving tags were used,", colored(nr_used_parasites, 'blue'), "parasite tags were used =>", colored(unknown, 'blue'), "unknown leave nodes")
-        print("Rootnode, Depths: [Min, Max, Mean], Originaltag, Finaltag, Nr_children")
-        print(nodelist[0])
-        # ---- reset countings ----
-        nr_leave_nodes = 0
-        nr_used_freelivings = 0
-        nr_used_parasites = 0
-        unknown = 0
-        nodelist = []
-        CURRENT_TIME = print_time(CURRENT_TIME)
+    print(colored("---------------- read tree ----------------", "green"))
+    subtree_path = '../data/subtree/Eukaryota.tre'
+    print("Build nodelist for: Eukaryota")
+    tree = Phylo.read(subtree_path, 'newick')
+    print(colored("---------------- tag tree ----------------", "green"))
+    current_freelivings = deepcopy(freelivings)
+    current_parasites = deepcopy(parasites)
+    fill_tree_with_tags(tree.clade, 0)
+    print(colored(nr_leave_nodes, 'blue'), "leave nodes are in the tree")
+    print(colored(nr_used_freelivings, 'blue'), "freeliving tags were used,", colored(nr_used_parasites, 'blue'), "parasite tags were used =>", colored(unknown, 'blue'), "unknown leave nodes")
+    print("Rootnode, Depth, Heigths: [Min, Max, Mean], Originaltag, Finaltag, Nr_children")
+    print(nodelist[0])
+    print(doubleTagged, "are tagged as P, but could also be FL!")
+    # ---- reset countings ----
+    nr_leave_nodes = 0
+    nr_used_freelivings = 0
+    nr_used_parasites = 0
+    unknown = 0
+    nodelist = []
     CURRENT_TIME = print_time(CURRENT_TIME)
     print(colored("--------------------------------", "green"))
     return
@@ -90,18 +87,19 @@ def read_tags(path):
         print('number of tag:', nr_tags)
     return tag_array
 
-def fill_tree_with_tags(subtree, subtree_name):
+def fill_tree_with_tags(subtree, depth):
     global nr_leave_nodes
     global nr_used_freelivings
     global nr_used_parasites
     global unknown
     global nodelist
+    global doubleTagged
     
     ott = subtree.name.split("$")[0] # remove index
-    depths = [1, 1, 1]
-    #              0    1       2           3           4
-    # nodelist - [id, depths, nr_children, originaltag, finaltag]
-    nodelist.append([ott, depths, len(subtree.clades), "", ""])
+    heights = [1, 1, 1]
+    #              0    1       2           3           4           5
+    # nodelist - [id, depth, heights, nr_children, originaltag, finaltag]
+    nodelist.append([ott, depth, heights, len(subtree.clades), "", ""])
     current_list_index = len(nodelist) - 1
 
     if subtree.is_terminal():
@@ -109,38 +107,40 @@ def fill_tree_with_tags(subtree, subtree_name):
         tag_boolp = get_tag(ott, 'P')
         if tag_boolp:
             nr_used_parasites += 1
-            nodelist[current_list_index][3] = "2"
+            nodelist[current_list_index][4] = "2"
+            if (get_tag(ott, 'FL')):
+                doubleTagged += 1
         else:
             tag_boolf = get_tag(ott, 'FL')
             if tag_boolf:
                 nr_used_freelivings += 1
-                nodelist[current_list_index][3] = "1"
+                nodelist[current_list_index][4] = "1"
             else:
-                nodelist[current_list_index][3] = "NA"
+                nodelist[current_list_index][4] = "NA"
                 unknown += 1
     else:
-        min_depth = float('inf')
-        max_depth = 0
-        mean_depth = 0
-        child_depth = 0
+        min_heigth = float('inf')
+        max_heigth = 0
+        mean_heigth = 0
+        child_heigth = 0
         for clade in subtree.clades:
-            depths = fill_tree_with_tags(clade, subtree_name)
-            if depths[0] < min_depth:
-                min_depth = depths[0]
-            if depths[1] > max_depth:
-                max_depth = depths[1]
-            child_depth = child_depth + depths[2]
-        mean_depth = child_depth/len(subtree.clades) + 1
-        depths = [min_depth + 1, max_depth + 1, mean_depth]
-        nodelist[current_list_index][1] = depths
+            heights = fill_tree_with_tags(clade, depth + 1)
+            if heights[0] < min_heigth:
+                min_heigth = heights[0]
+            if heights[1] > max_heigth:
+                max_heigth = heights[1]
+            child_heigth = child_heigth + heights[2]
+        mean_heigth = child_heigth/len(subtree.clades) + 1
+        heights = [min_heigth + 1, max_heigth + 1, mean_heigth]
+        nodelist[current_list_index][2] = heights
     # -------------------------------------------------
-    csv_title = '../data/nodelist/' + subtree_name + '.csv' 
+    csv_title = '../data/nodelist/Eukaryota.csv' 
     nodelist_file = open(csv_title, 'a')
     writer = csv.writer(nodelist_file)
     writer.writerow((nodelist[current_list_index])) 
     nodelist_file.close()
     # -------------------------------------------------
-    return depths
+    return heights
 
 def get_tag(name, tag):
     global current_freelivings
