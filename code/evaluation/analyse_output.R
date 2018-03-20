fd17de78d208c651448b382cce9f7b84d282d61d
MIRACULIX\Irallia <irallia@chrigelyra.de>
2018-03-20 02:05:55 +0100

    Paper - Results - Biological View

--- code/evaluation/analyse_output.R
+++ code/evaluation/analyse_output.R
@@ -4,6 +4,7 @@ library(data.table)
 format(Sys.time(), "%a %b %d %X %Y")
 
 all.taxa <- fread("../../results/Eukaryota-taxa.csv")
+# ott_id,originaltag,finaltag,depth,heights,nr_children,name,rank,uniqname,kingdom,phylum,class,order,family
 
 ### right mode for all
 all.taxa$kingdom <- as.factor(all.taxa$kingdom)
@@ -20,121 +21,98 @@ names(heights) <- c("min.height", "max.height", "mean.height")
 
 all.taxa <- cbind(all.taxa, heights)
 
-## list all Kingdoms, Phyla:
-# table(all.taxa$kingdom)
-# table(all.taxa$phylum)
-
-## which Phylum in which Kingdom:
-# table(all.taxa$kingdom, all.taxa$phylum)
-
-### all nodes sorted by rank
-# table(all.taxa$rank)[order(table(all.taxa$rank))]
-
-print('------------all.taxa$originaltag------------------')
-table(all.taxa$originaltag)
-print('------------all.taxa$finaltag------------------')
-table(all.taxa$finaltag)
-
-# table(all.taxa[all.taxa$finaltag+1 != all.taxa$originaltag, ]$kingdom)
-
-print('---------------------------')
-
-table(droplevels(all.taxa[all.taxa$finaltag+1 != all.taxa$originaltag, ]$phylum))
-
-print('-----------Ascomycota-------------')
-ascomycota <- all.taxa[all.taxa$phylum=="['ott439373', 'Ascomycota']", ]
-bug_data.ascomycota <- ascomycota[ascomycota$finaltag+1 != ascomycota$originaltag, ]
-table(droplevels(bug_data.ascomycota$class))
-dim(ascomycota)
-
-print('---------------------------')
-
-print('-----------Chordata-------------')
-chordata <- all.taxa[all.taxa$phylum=="['ott125642', 'Chordata']", ]
-bug_data.chordata <- chordata[chordata$finaltag+1 != chordata$originaltag, ]
-table(droplevels(bug_data.chordata$class))
-dim(chordata)
-
-print('-----------Sarcopterygii-------------')
-sarcopterygii <- chordata[chordata$class=="['ott458402', 'Sarcopterygii']", ]
-bug_data.sarcopterygii <- sarcopterygii[sarcopterygii$finaltag+1 != sarcopterygii$originaltag, ]
-table(droplevels(bug_data.sarcopterygii$order))
-dim(sarcopterygii)
-
-print('---------------------------')
-
-print('-----------Arthropoda-------------')
-arthropoda <- all.taxa[all.taxa$phylum=="['ott632179', 'Arthropoda']", ]
-bug_data.arthropoda <- arthropoda[arthropoda$finaltag+1 != arthropoda$originaltag, ]
-table(droplevels(bug_data.arthropoda$class))
-
-print('-----------Insecta-------------')
-insecta <- arthropoda[arthropoda$class=="['ott1062253', 'Insecta']", ]
-bug_data.insecta <- insecta[insecta$finaltag+1 != insecta$originaltag, ]
-table(droplevels(bug_data.insecta$order))
-
-diptera <- insecta[insecta$order=="['ott661378', 'Diptera']", ]
-dim(diptera)
-
-hemiptera <- insecta[insecta$order=="['ott603650', 'Hemiptera']", ]
-dim(hemiptera)
-
-# coleoptera <- insecta[insecta$order=="['ott865243', 'Coleoptera']", ]
-# dim(coleoptera)
-
-# lepidoptera <- insecta[insecta$order=="['ott965954', 'Lepidoptera']", ]
-# dim(lepidoptera)
-
-hymenoptera <- insecta[insecta$order=="['ott753726', 'Hymenoptera']", ]
-dim(hymenoptera)
-# bug_data.hymenoptera <- hymenoptera[hymenoptera$finaltag+1 != hymenoptera$originaltag, ]
-# print(bug_data.hymenoptera)
-
+# print('------------all.taxa$originaltag------------------')
+# table(all.taxa$originaltag)
+# print('------------all.taxa$finaltag------------------')
+# table(all.taxa$finaltag)
 
 print('--------------------------------------------------')
 
-# leaf.taxa <-  all.taxa[all.taxa$nr_children==0, ]
-# extendedLeaf.taxa <- all.taxa[all.taxa$nr_children<2 & min.height==max.height, ]
-# inner.taxa <- all.taxa[all.taxa$nr_children>1 | (all.taxa$nr_children==1 & min.height!=max.height), ]
+leaf.taxa <-  all.taxa[all.taxa$nr_children==0, ]
 
-# print('----------------------- leaf.taxa$rank ---------------------------')
-# leaf.taxa <- as.data.frame(leaf.taxa)
-# # table(leaf.taxa$rank)[order(table(leaf.taxa$rank))]
-# print('----------------------- extendedLeaf.taxa$rank ---------------------------')
-# extendedLeaf.taxa <- as.data.frame(extendedLeaf.taxa)
-# # table(extendedLeaf.taxa$rank)[order(table(extendedLeaf.taxa$rank))]
-# print('----------------------- inner.taxa$rank ---------------------------')
-# inner.taxa <- as.data.frame(inner.taxa)
-# # table(inner.taxa$rank)[order(table(inner.taxa$rank))]
-# print('--------------------------------------------------')
-
-# tapply(inner.taxa$max.height, inner.taxa$rank, summary)
-
-
-# print('-----------------------leaf.taxa$originaltag---------------------------')
-# table(leaf.taxa$originaltag, leaf.taxa$rank)
-# print('--------------------------------------------------')
+print('--------------------------------------------------')
 
 
 
-# leaf.taxa$globi.data <- !is.na(leaf.taxa$originaltag)
 
-# print('-----------Metazoa-------------')
-# metazoa <- all.taxa[all.taxa$kingdom=="['ott691846', 'Metazoa']", ]
+# print('-------------Are the changed originaltags?--------------')
+# table(leaf.taxa[leaf.taxa$finaltag+1 != leaf.taxa$originaltag, ]$kingdom)
+
+
+print('-----------Analyse some Phyla about their prediciton-------------')
+print('-----------Metazoa-------------')
+metazoa <- leaf.taxa[leaf.taxa$kingdom=="['ott691846', 'Metazoa']", ]
 # # table(metazoa$phylum, metazoa$originaltag)
+print('--------Leaf nodes:------------')
+# table(droplevels(all.taxa[all.taxa$nr_children==0, ]$phylum))
+print('----------Parasites / Free-livings - original states:---------------')
 
-# print('-----------Nematoda-------------')
-# nematoda <- all.taxa[all.taxa$phylum=="['ott395057', 'Nematoda']", ]
-# # table(nematoda$order, nematoda$originaltag)
-# # table(nematoda$rank)
+print('-----------Chordata-------------')
+chordata <- metazoa[metazoa$phylum=="['ott125642', 'Chordata']", ]
+table(chordata$originaltag)
+print('----------final:--------------')
+table(chordata$finaltag)
 
-# print('-----------Chordata-------------')
-# chordata <- all.taxa[all.taxa$phylum=="['ott125642', 'Chordata']", ]
-# # chordata[chordata$originaltag==2, ]
+print('-----------Nematoda-------------')
+nematoda <- metazoa[metazoa$phylum=="['ott395057', 'Nematoda']", ]
+table(nematoda$originaltag)
+print('----------final:--------------')
+table(nematoda$finaltag)
 
-# print('-----------Platyhelminthes-------------')
-# platyhelminthes <- all.taxa[all.taxa$phylum=="['ott555379', 'Platyhelminthes']", ]
-# # platyhelminthes[platyhelminthes$originaltag==1, ]
+print('-----------Platyhelminthes-------------')
+platyhelminthes <- metazoa[metazoa$phylum=="['ott555379', 'Platyhelminthes']", ]
+table(platyhelminthes$originaltag)
+print('----------final:--------------')
+table(platyhelminthes$finaltag)
+print('------------------------')
+# table(platyhelminthes[platyhelminthes$finaltag==0, ]$name)
+
+print('-----------Apicomplexa-------------')
+apicomplexa <- leaf.taxa[leaf.taxa$phylum=="['ott422673', 'Apicomplexa']", ]
+table(apicomplexa$originaltag)
+table(apicomplexa[apicomplexa$originaltag==1, ]$name)
+
+print('----------final:--------------')
+table(apicomplexa$finaltag)
+print('------------------------')
+table(apicomplexa[apicomplexa$finaltag==0, ]$name)
+
+
+
+print('-------------More about Chordata:-----------')
+print('--------Classes with parasites:-------')
+table(droplevels(chordata[chordata$finaltag==1, ]$class))
+Actinopteri <- chordata[chordata$class=="['ott285821', 'Actinopteri']", ]
+Sarcopterygii <- chordata[chordata$class=="['ott458402', 'Sarcopterygii']", ]
+print('--------Orders with parasites:-------')
+table(droplevels(Actinopteri[Actinopteri$originaltag==2, ]$order))
+table(droplevels(Actinopteri[Actinopteri$finaltag==1, ]$order))
+print('------------')
+table(droplevels(Sarcopterygii[Sarcopterygii$originaltag==2, ]$order))
+table(droplevels(Sarcopterygii[Sarcopterygii$finaltag==1, ]$order))
+
+Cypriniformes <- chordata[chordata$order=="['ott1005931', 'Cypriniformes']", ]
+Sauria <- chordata[chordata$order=="['ott329823', 'Sauria']", ]
+Anura <- chordata[chordata$order=="['ott991547', 'Anura']", ]
+Acanthomorphata <- chordata[chordata$order=="['ott108720', 'Acanthomorphata']", ]
+Siluriformes <- chordata[chordata$order=="['ott701516', 'Siluriformes']", ]
+print('--------Families with parasites:-------')
+
+table(Cypriniformes[Cypriniformes$finaltag==1, ]$family)
+table(Cypriniformes[Cypriniformes$finaltag==1, ]$name)
+print('------------------------')
+table(Sauria[Sauria$finaltag==1, ]$family)
+print('------------------------')
+table(Anura[Anura$finaltag==1, ]$name)
+print('------------------------')
+table(Acanthomorphata[Acanthomorphata$finaltag==1, ]$name)
+Blenniidae <- Acanthomorphata[Acanthomorphata$family=="['ott867524', 'Blenniidae']", ]
+table(Blenniidae[Blenniidae$finaltag==1, ]$name)
+print('------------------------')
+table(Siluriformes[Siluriformes$finaltag==1, ]$name)
+print('------------------------')
+table(Sauria[Sauria$finaltag==1, ]$family)
+table(Sauria[Sauria$finaltag==1, ]$name)
 
 
 
